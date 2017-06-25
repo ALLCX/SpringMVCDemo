a8495b3fa887f8611197f6bc93e34955669274ad
A.L <late_enrollee@126.com>
2017-06-25 23:31:34 +0800

    new project

---
+++ src/com/jackie/springmvc/controllers/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.jackie.springmvc.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloWorldController implements Controller{
+	
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("message", "Hello World!");
+		mv.setViewName("hello");
+		
+		return mv;
+	}
+}
